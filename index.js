bbef55ec65f937933df73291400522a40aba9080
BGZhao <92346786+BGZhao@users.noreply.github.com>
2022-01-05 17:02:32 -0600

    add changes
    Add Changes index.js and move.js

--- index.js
+++ index.js
@@ -1,23 +1,97 @@
-function newImage(image,left,bottom){
-    let img = document.createElement('img')
-   img.src = 'assets/' + image
-   img.style.position = 'fixed'
-   img.style.left = left
-   img.style.bottom = bottom
-   document.body.append(img)
-   img.addEventListener('dblclick', function(){
-    img.remove()
-})
+
+const inventory = newInventory()
+move(inventory).to(0, 0)
+
+const character = newImage('assets/green-character/static.gif')
+
+function handleDirectionChange(direction){
+    if(direction === null){
+        character.src = 'assets/green-character/static.gif'
+    }
+    if(direction === 'west'){
+        character.src = 'assets/green-character/west.gif'
+    }
+    if(direction === 'north'){
+        character.src = 'assets/green-character/north.gif'
+    }
+    if(direction === 'east'){
+        character.src = 'assets/green-character/east.gif'
+    }
+    if(direction === 'south'){
+        character.src = 'assets/green-character/south.gif'
+    }
 }
-newImage('green-character.gif','100px','100px')
-newImage('pine-tree.png', '450px','200px')
-newImage('tree.png','200px','300px')
-newImage('pillar.png','350px','100px')
-newImage('crate.png','150px','200px')
-newImage('well.png','500px','425px')
-newImage('sword.png','500px','405px') 
-newImage('shield.png','165px','185px')
-newImage('staff.png','600px','100px')
 
-document.body.style.backgroundImage = "url('assets/grass.png')";
+move(character).withArrowKeys(100, 250, handleDirectionChange)
 
+function move(element) {
+    element.style.position = 'fixed'
+
+    function moveToCoordinates(left, bottom) {
+        element.style.left = left + 'px'
+        element.style.bottom = bottom + 'px'
+    }
+
+    function moveWithArrowKeys(left, bottom, callback){
+        let direction = null;
+        let x = left;
+        let y = bottom;
+       
+    
+        element.style.left = x + 'px'
+        element.style.bottom = y + 'px'
+        
+        function moveCharacter(){ 
+            if(direction === 'west'){
+                x-=1
+            }
+            if(direction === 'north'){
+                y+=1
+            }
+            if(direction === 'east'){
+                x+=1
+            }
+            if(direction === 'south'){
+                y-=1
+            }
+            element.style.left = x + 'px'
+            element.style.bottom = y + 'px'
+        }
+        
+        setInterval(moveCharacter, 1)
+        
+        document.addEventListener('keydown', function(e){
+            if(e.repeat) return;
+        
+            if(e.key === 'ArrowLeft'){
+                direction = 'west'
+            }
+            if(e.key === 'ArrowUp'){
+                direction = 'north'
+            }
+            if(e.key === 'ArrowRight'){
+                direction = 'east'
+            }
+            if(e.key === 'ArrowDown'){
+                direction = 'south'
+            }
+        })
+        
+        document.addEventListener('keyup', function(e){
+            direction = null
+        })
+    }
+
+    return {
+        to: moveToCoordinates,
+        withArrowKeys: moveWithArrowKeys
+    }        
+}
+move(newImage('assets/tree.png')).to(200, 450)
+move(newImage('assets/pillar.png')).to(350, 250)
+move(newImage('assets/pine-tree.png')).to(450, 350)
+move(newImage('assets/crate.png')).to(150, 350)
+move(newImage('assets/well.png')).to(500, 575)
+move(newItem('assets/sword.png')).to(500, 555)
+move(newItem('assets/shield.png')).to(165, 335)
+move(newItem('assets/staff.png')).to(600, 250)
